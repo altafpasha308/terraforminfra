6f1df14d06f81d7bd9f2b4f9f09ed3356a282d37
Ubuntu <altafpasha308@gamil.com>
2026-02-22 06:41:06 +0000

    Modified VMs to have password

--- azure/vm.tf
+++ azure/vm.tf
@@ -5,16 +5,14 @@ resource "azurerm_linux_virtual_machine" "web_vm" {
   location            = azurerm_resource_group.resource_group.location
   size                = "Standard_B2als_v2" # Free tier / Cheap eligible
   admin_username      = "azureuser"
+  admin_password      = "Thisistraining@123"
+  disable_password_authentication = false
 
   network_interface_ids = [
     azurerm_network_interface.web_nic[count.index].id,
   ]
 
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-
+  
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
